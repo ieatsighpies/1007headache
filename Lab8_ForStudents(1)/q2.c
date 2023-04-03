af7a4014fbf570b94357846363aa2d9419bbdd03
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-04-03 14:32:36 +0800

    modified:   Lab8_ForStudents(1)/q2.c 	modified:   Lab8_ForStudents(1)/q3.c commit changes

--- Lab8_ForStudents(1)/q2.c
+++ Lab8_ForStudents(1)/q2.c
@@ -3,21 +3,36 @@
 
 //memory
 int *r;
+void print_pos(int rlength, int first_cut[]){
+    if(rlength==0) return;
 
+    int pos = first_cut[rlength];
+    printf("%d ",pos);
+    print_pos(rlength-pos, first_cut);
+}
 
 int cr_bottom_up_dp_print(int *p, int n)
 {
     //write your code here
     int i,j;
     r[0]=0;
+    int first_cut[n+1];
+
     for(i=1;i<=n;i++){
+        int pos_temp=0;
+
         for(j=1;j<=i;j++){
             int temp = p[j]+r[i-j];
             if(temp > r[i]){
+                pos_temp = j;
                 r[i] = temp;
             }
         }
+        first_cut[i] = pos_temp;
     }
+    printf("Length of each piece is: \n");
+    print_pos(n,first_cut);
+    printf("\n");
     return r[n];
 }
 
--- Lab8_ForStudents(1)/q3.c
+++ Lab8_ForStudents(1)/q3.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int waysToScore(int n, int* score){
+int waysToScore(int n, int* score)
 {
     //write your code here
+    // if(n==0) return 0;
+
+    // for(int i=0;i<n;i++){
+    //     if(n>=10)
+    //         n-=score[2];
+    //     else if(n>=5)
+    //         n-=score[1];
+    //     else if(n>=3)
+    //         n-=score[0];
+    // }
+    int* arr;
+    arr = (int*) malloc(sizeof(int)*n);
+    int i;
+    for(i=0;i<n;i++) arr[i]=0;
+
+    arr[0] = 1;
+
+    for(i=1;i<=n;i++){
+        if(i-score[0]>=0) arr[i]+=arr[i-score[0]];
+        if(i-score[1]>=0) arr[i] +=arr[i-score[1]];
+        if(i-score[2]>=0) arr[i] +=arr[i-score[2]];
+    }
+    return arr[n];
 }
 
- 
+
 void main ()
 {
     int n;
 
-    int score[3] = {3,5,10}; 
+    int score[3] = {3,5,10};
 
     printf("Enter the value of N: ");
     scanf("%d",&n);
